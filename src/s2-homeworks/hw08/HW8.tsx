69117c2682343e4717074c9c2d17735823914280
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2023-04-29 00:48:44 +0300

    hw 9

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -1,9 +1,9 @@
-import React, {useState} from 'react'
-import {homeWorkReducer} from './bll/homeWorkReducer'
-import s from './HW8.module.css'
-import s2 from '../../s1-main/App.module.css'
-import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
-import User from './User'
+import React, {useState} from "react"
+import {homeWorkReducer} from "./bll/homeWorkReducer"
+import s from "./HW8.module.css"
+import s2 from "../../s1-main/App.module.css"
+import SuperButton from "../hw04/common/c2-SuperButton/SuperButton"
+import User from "./User"
 
 
 export type UserType = {
@@ -13,70 +13,71 @@ export type UserType = {
 }
 
 const initialPeople: UserType[] = [
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
+    {_id: 0, name: "Кот", age: 3},
+    {_id: 1, name: "Александр", age: 66},
+    {_id: 2, name: "Коля", age: 16},
+    {_id: 3, name: "Виктор", age: 44},
+    {_id: 4, name: "Дмитрий", age: 40},
+    {_id: 5, name: "Ирина", age: 55},
 ]
 
 const HW8 = () => {
     const [people, setPeople] = useState<UserType[]>(initialPeople)
-    const [currentSort, setCurrentSort] = useState('')
+    const [currentSort, setCurrentSort] = useState("")
 
     const finalPeople = people.map((u: UserType) => <User key={u._id} u={u}/>)
 
     const sortUp = () => {
         setPeople(
-            homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'})
+            homeWorkReducer(initialPeople, {type: "sort", payload: "up"})
         )
-        setCurrentSort('up')
+        setCurrentSort("up")
     }
 
     const sortDown = () => {
         setPeople(
-            homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'})
+            homeWorkReducer(initialPeople, {type: "sort", payload: "down"})
         )
-        setCurrentSort('down')
+        setCurrentSort("down")
     }
     const check18 = () => {
         setPeople(
-            homeWorkReducer(initialPeople, {type: 'check', payload: 18})
+            homeWorkReducer(initialPeople, {type: "check", payload: 18})
         )
-        setCurrentSort('18')
+        setCurrentSort("18")
     }
 
     return (
-        <div id={'hw3'}>
+        <div id={"hw3"}>
             <div className={s2.hwTitle}>Homework #8</div>
+            <hr/>
             <div className={s2.hw}>
                 <div className={s.container}>
                     <div className={s.buttonsContainer}>
                         <SuperButton
-                            id={'hw8-button-up'}
+                            id={"hw8-button-up"}
                             onClick={sortUp}
-                            xType={currentSort === 'up' ? '' : 'secondary'}
+                            xType={currentSort === "up" ? "" : "secondary"}
                         >
                             Sort up
                         </SuperButton>
                         <SuperButton
-                            id={'hw8-button-down'}
+                            id={"hw8-button-down"}
                             onClick={sortDown}
-                            xType={currentSort === 'down' ? '' : 'secondary'}
+                            xType={currentSort === "down" ? "" : "secondary"}
                         >
                             Sort down
                         </SuperButton>
                         <SuperButton
-                            id={'hw8-button-18'}
+                            id={"hw8-button-18"}
                             onClick={check18}
-                            xType={currentSort === '18' ? '' : 'secondary'}
+                            xType={currentSort === "18" ? "" : "secondary"}
                         >
                             Check 18+
                         </SuperButton>
                     </div>
 
-                    <table id={'hw8-users'} className={s.users}>
+                    <table id={"hw8-users"} className={s.users}>
                         <thead className={s.thead}>
                         <tr>
                             <td className={s.nameCol}>Name</td>
@@ -88,6 +89,7 @@ const HW8 = () => {
                     </table>
                 </div>
             </div>
+            <hr/>
         </div>
     )
 }
--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -1,59 +1,64 @@
-import React, {useState} from 'react'
-import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
-import {restoreState} from '../hw06/localStorage/localStorage'
-import s from './Clock.module.css'
+import React, {useState} from "react"
+import SuperButton from "../hw04/common/c2-SuperButton/SuperButton"
+import {restoreState} from "../hw06/localStorage/localStorage"
+import s from "./Clock.module.css"
+
 
 function Clock() {
     const [timerId, setTimerId] = useState<number | undefined>(undefined)
-    // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
-    const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
+    const [date, setDate] = useState<Date>(new Date(restoreState("hw9-date", Date.now())))
     const [show, setShow] = useState<boolean>(false)
 
     const start = () => {
-        // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
-        // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
-
+        let idTimer: number = window.setInterval(() => {
+            setDate(new Date(Date.now()))
+        }, 1000)
+        setTimerId(idTimer)
     }
-
     const stop = () => {
-        // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
-
+        clearInterval(timerId)
+        setTimerId(undefined)
     }
 
-    const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
-
-    }
-    const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
-
+    const onMouseEnter = () => {
+        setShow(true)
     }
 
-    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const onMouseLeave = () => {
+        setShow(false)
+    }
 
-    // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
+    const zero = (num: number) => num < 10 ? "0" + num : num
+
+    const stringTime = `${zero(date.getHours())}:${zero(date.getMinutes())}:${zero(date.getSeconds())}` ||
+        <br/>
+    const stringDate = `${zero(date.getDate())}.${zero(date.getMonth() + 1)}.${date.getFullYear()}` || <br/> ||
+        <br/>
+
+
+    const stringDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] || <br/>
+    const stringMonth =
+        ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"] ||
+        <br/>
 
     return (
         <div className={s.clock}>
             <div
-                id={'hw9-watch'}
+                id={"hw9-watch"}
                 className={s.watch}
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
-                <span id={'hw9-day'}>{stringDay}</span>,{' '}
-                <span id={'hw9-time'}>
-                    <strong>{stringTime}</strong>
-                </span>
+                <span id={"hw9-day"}>{stringDay[date.getDay()]}</span>,
+                {" "}
+                <span id={"hw9-time"}><strong>{stringTime}</strong></span>
             </div>
-
-            <div id={'hw9-more'}>
+            <div id={"hw9-more"}>
                 <div className={s.more}>
                     {show ? (
                         <>
-                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
-                            <span id={'hw9-date'}>{stringDate}</span>
+                            <span id={"hw9-month"}>{stringMonth[date.getMonth()]}</span>,{" "}
+                            <span id={"hw9-date"}>{stringDate}</span>
                         </>
                     ) : (
                         <>
@@ -65,15 +70,15 @@ function Clock() {
 
             <div className={s.buttonsContainer}>
                 <SuperButton
-                    id={'hw9-button-start'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер запущен
+                    id={"hw9-button-start"}
+                    disabled={!!timerId}
                     onClick={start}
                 >
                     start
                 </SuperButton>
                 <SuperButton
-                    id={'hw9-button-stop'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер не запущен
+                    id={"hw9-button-stop"}
+                    disabled={!timerId}
                     onClick={stop}
                 >
                     stop
